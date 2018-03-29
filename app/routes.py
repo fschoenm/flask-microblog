6e73cecbbd5212f3d8f4688617c3dda400ec940d
Frank Schoenmann <fschoenm@gmail.com>
2018-03-29 23:08:09 +0200

    More fun with templates.

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'fschoenm'}
-    return render_template('index.html', title='Home', user=user)
+    # mock data
+    user = {'username': 'Frank'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+    ]
+
+    # render page
+    return render_template('index.html', title='Home', user=user, posts=posts)
